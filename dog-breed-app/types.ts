7f1b84d849be4554340b83eca901c644d00666e6
MULX <mulxman@gmail.com>
2023-08-28 21:10:16 +1000

    State store added

---
+++ dog-breed-app/types.ts
@@ -0,0 +1,18 @@
+// types.ts
+export interface Breed {
+    name: string;
+    subBreeds: string[];
+  }
+  
+  export interface BreedImage {
+    breed: string;
+    image: string;
+  }
+  
+  export interface AppState {
+    selectedBreeds: string[];
+    maxImages: number;
+    breedList: Breed[];
+    breedImages: Record<string, string[]>;
+  }
+  
